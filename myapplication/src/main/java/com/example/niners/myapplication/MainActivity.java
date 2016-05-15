bd5ec4edb477f8b0aaaf1f2e9f7f1ae55ae2d851
jweissen <jweiss12@gmail.com>
2016-05-14 22:38:08 -0500

    fixed library name so it would play nice with maven

---
+++ myapplication/src/main/java/com/example/niners/myapplication/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.niners.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.jw.android.huddroid.HUDDroid;
+import com.jw.android.huddroid.MaskType;
+import com.jw.android.huddroid.TimeSpan;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Show a simple status message with an indeterminate spinner
+        //AndHUD.Shared.Show(myActivity, "Status Message", MaskType.Clear);
+/**
+ HUDDroid.getInstance().build(this)
+ .setMessage("Status Message")
+ .setMaskType(MaskType.Clear)
+ .show();
+
+ //Show a progress with a filling circle representing the progress amount
+ //AndHUD.Shared.ShowProgress(myActivity, "Loading… 60%", 60);
+ HUDDroid.getInstance().build(this, 60)
+ .setMessage("Loading... 60%")
+ .show();
+
+
+ //Show a success image with a message
+ //AndHUD.Shared.ShowSuccess(myActivity, "It Worked!", MaskType.Clear, TimeSpan.FromSeconds(2));
+ HUDDroid.getInstance().buildSuccess(this)
+ .setMessage("It Worked!")
+ .setMaskType(MaskType.Clear)
+ .setTimeSpan(TimeSpan.fromSeconds(2))
+ .show();
+
+
+        //Show an error image with a message
+        //AndHUD.Shared.ShowError(myActivity, "It no worked :()", MaskType.Black, TimeSpan.FromSeconds(2));
+        HUDDroid.getInstance().buildFailureWithStatus(this, "It no worked :()")
+                .setMaskType(MaskType.Black)
+                .setTimeSpan(TimeSpan.fromSeconds(2))
+                .show();
+ */
+
+
+//
+////Show a toast, similar to Android native toasts, but styled as AndHUD
+//        AndHUD.Shared.ShowToast(myActivity, "This is a non-centered Toast…", MaskType.Clear, TimeSpan.FromSeconds(2));
+//
+////Show a custom image with text
+//        AndHUD.Shared.ShowImage(myActivity, Resource.Drawable.MyCustomImage, "Custom");
+//
+////Dismiss a HUD that will or will not be automatically timed out
+//        AndHUD.Shared.Dismiss(myActivity);
+//
+////Show a HUD and only close it when it's clicked
+//        AndHUD.Shared.ShowToast(this, "Click this toast to close it!", MaskType.Clear, null, true, () => AndHUD.Shared.Dismiss(this));
+
+        HUDDroid.getInstance().buildToast(this, "Click this toast to close it!")
+                .setMaskType(MaskType.Clear)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        HUDDroid.getInstance().dismissCurrent();
+                    }
+                }).show();
+
+    }
+}
